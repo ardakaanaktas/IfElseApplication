5dbc41dbf7ddf1a623eead131797b47b00bbb25b
Arda Kaan AKTAS <arda_kaan_aktas@yahoo.com>
2024-10-03 11:25:58 +0300

    Yorum satırları eklendi

--- IfElseApplication/Program.cs
+++ IfElseApplication/Program.cs
@@ -4,30 +4,38 @@
 	{
 		static void Main(string[] args)
 		{
-            Console.Write("Lutfen bir sayi giriniz: ");
+			Console.Write("Lutfen bir sayi giriniz: ");
 			int sayi = Convert.ToInt32(Console.ReadLine());
 
-            if (sayi > 10)
-            {
-                Console.WriteLine("Girilen sayi 10'dan buyuktur.");
-            }
-			else if (sayi < 10)
+			if (sayi > 10) // Girilen sayi 10'dan buyukse
+			{
+				Console.WriteLine("Girilen sayi 10'dan buyuktur.");
+			}
+			else if (sayi < 10) // Girilen sayi 10'dan kucukse
 			{
 				Console.WriteLine("Girilen sayi 10'dan kucuktur.");
 			}
-			else
+			else // Girilen sayi 10'a esitse
 			{
 				Console.WriteLine("Girilen sayi 10'a esittir.");
 			}
 
-			if (sayi % 2 == 0)
+			if (sayi % 2 == 0) // Girilen sayi ciftse
 			{
 				Console.WriteLine("Girilen sayi cifttir.");
 			}
-			else
+			else // Girilen sayi tekse
 			{
 				Console.WriteLine("Girilen sayi tektir.");
 			}
+
+
 		}
 	}
 }
+			
+
+			
+		
+	
+
