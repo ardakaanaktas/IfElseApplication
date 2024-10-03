3a0f5386fb8d32681907dc8e082ba6fde6d2e19c
Arda Kaan AKTAS <arda_kaan_aktas@yahoo.com>
2024-10-03 11:12:33 +0300

    Proje dosyası ekle.

---
+++ IfElseApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IfElseApplication
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.Write("Lutfen bir sayi giriniz: ");
+			int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 10)
+            {
+                Console.WriteLine("Girilen sayi 10'dan buyuktur.");
+            }
+			else if (sayi < 10)
+			{
+				Console.WriteLine("Girilen sayi 10'dan kucuktur.");
+			}
+			else
+			{
+				Console.WriteLine("Girilen sayi 10'a esittir.");
+			}
+
+			if (sayi % 2 == 0)
+			{
+				Console.WriteLine("Girilen sayi cifttir.");
+			}
+			else
+			{
+				Console.WriteLine("Girilen sayi tektir.");
+			}
+		}
+	}
+}
